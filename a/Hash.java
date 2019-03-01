4cad5b38fd86fc7f385ffa54008c0b5bacdde9a5
Janez Justin <janez.justin0@gmail.com>
2019-03-01 21:33:00 +0100

    ful pocasn, podvoji pike na b primeru

--- a/Hash.java
+++ a/Hash.java
@@ -52,6 +52,7 @@ public class Hash {
             //System.out.println(Arrays.deepToString(vseSlike.toArray()));
             photo = findNextPhoto(tags);
             if(photo.isVertical()){
+                photo.isUsed = true;
                 Photo photo2 = poisciNajboljsoVertikalko(photo);
                 SlideShow.add(photo,photo2);
                 tags = photo.tags;
@@ -63,30 +64,40 @@ public class Hash {
         }
     }
 
-    public static ArrayList<String> skupniTagi(Photo p, ArrayList<String> tags){
-        // ArrayList<String> l = new ArrayList<String>(slike.keySet());
-        ArrayList<String> a = p.tags;
+    public static int skupniTagi(Photo p, ArrayList<String> tags){
+        ArrayList<String> a = new ArrayList<>(p.tags);
         a.retainAll(tags);
-        // for(String s : a){
-        //     l.remove(s);
-        // }
-
-        return a;
+        // System.out.println(tags.size() + " " + a.size() + " " + p.tags.size());
+        return a.size();
     }
 
     public static Photo findNextPhoto(ArrayList<String> tags) {
         Photo maxP = null;
+        int maxTock = 0;
         
-        int maxI = tags.size();
+        int stTagov = tags.size();
 
         for(String tag : tags){
             ArrayList<Photo> photos = slike.get(tag);
             for(Photo s : photos){
-                if(!s.isUsed){
-                    int i = skupniTagi(s, tags).size();
-                    if(i != 0) return s;
+                if(s.isUsed){ 
+                    continue;
+                }
+                int presek = skupniTagi(s, tags);
+                if(presek == 0) continue;
+                int slikaStTagov = s.tags.size();
+                int samoSlika = slikaStTagov - presek;
+                int samoOriginal = stTagov - presek;
+                int tocke = Math.min(samoSlika, Math.min(samoOriginal, presek));
+                // System.out.println(samoOriginal + " " + presek + " "+ slikaStTagov);
+                if(tocke > maxTock){
+                    maxP = s;
+                    maxTock = tocke;
                 }
             }
+        }
+        if(maxP != null) {
+            return maxP;
         }
 
         for(Photo s : vseSlike){
@@ -106,7 +117,7 @@ public class Hash {
         Photo maxP = null;
         for(Photo p : vseSlike){
             if(p.isUsed || !p.vertical) continue;
-            int size = skupniTagi(p, slika1.tags).size();
+            int size = skupniTagi(p, slika1.tags);
             if(size == 0) return p;
             if(minSize > size) {
                 minSize = size;
